152dc116dc8783a7bf7fc2093af9e3d2409d8d9a
admin <>
2023-07-27 11:28:46 +0000

    {/src/main/java/com/drools_space/cron_job_rule/CallRestApi.java}

--- src/main/java/com/drools_space/cron_job_rule/CallRestApi.java
+++ src/main/java/com/drools_space/cron_job_rule/CallRestApi.java
@@ -6,29 +6,41 @@ package com.drools_space.cron_job_rule;
 
 public class CallRestApi implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public CallRestApi() {
-    }
+	private java.lang.String id;
 
+	public CallRestApi() {
+	}
 
-    public static String getMethod() {
+	public static String getMethod() {
 		HttpRequest request = HttpRequest.newBuilder()
 				.uri(URI.create("https://gorest.co.in/public/v2/todos"))
-				.method("GET", HttpRequest.BodyPublishers.noBody())
-				.build();
+				.method("GET", HttpRequest.BodyPublishers.noBody()).build();
 		HttpResponse<String> response = null;
 		try {
-			response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+			response = HttpClient.newHttpClient().send(request,
+					HttpResponse.BodyHandlers.ofString());
 			return response.body();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
+
 		return null;
 	}
 
+	public java.lang.String getId() {
+		return this.id;
+	}
+
+	public void setId(java.lang.String id) {
+		this.id = id;
+	}
+
+	public CallRestApi(java.lang.String id) {
+		this.id = id;
+	}
 
 }
